c7ceff476cbbb84406ae1cddc655e05ad2089c06
Kerem-Kaplan <keremkaplan1000@gmail.com>
2023-03-23 03:53:24 +0300

    added login and register panel. created category ui and listed

---
+++ app/src/main/java/dev/krm/androideatit/Common/Common.java
@@ -0,0 +1,8 @@
+package dev.krm.androideatit.Common;
+
+import dev.krm.androideatit.Model.User;
+
+public class Common {
+    public static User currentUser;
+
+}
---
+++ app/src/main/java/dev/krm/androideatit/ViewHolder/MenuViewHolder.java
@@ -0,0 +1,36 @@
+package dev.krm.androideatit.ViewHolder;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import dev.krm.androideatit.Interface.ItemClickListener;
+import dev.krm.androideatit.R;
+
+public class MenuViewHolder extends  RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView txtMenuName;
+    public ImageView imageView;
+
+    private ItemClickListener itemClickListener;
+
+    public MenuViewHolder(@NonNull View itemView) {
+        super(itemView);
+        txtMenuName=(TextView) itemView.findViewById(R.id.menu_name);
+        imageView=(ImageView) itemView.findViewById(R.id.menu_image);
+
+        itemView.setOnClickListener(this);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener){
+        this.itemClickListener=itemClickListener;
+    }
+
+    @Override
+    public void onClick(View view) {
+itemClickListener.onClick(view,getAdapterPosition(),false);
+    }
+}
